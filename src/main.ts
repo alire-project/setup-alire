c1cb11807c89e6452a168ad2d3f9793428c5ea15
Fabien Chouteau <chouteau@adacore.com>
2019-09-05 19:05:32 +0200

    First version

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+const path = require("path");
+
+const repo_url="https://github.com/alire-project/alire.git";
+const repo_branch="master";
+const alire_src="alire_src";
 
 async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    try {
+        if (process.platform != 'linux') {
+            core.setFailed(`Alire is not available on ${process.platform} yet`);
+            return;
+        }
+
+        await exec.exec('sudo apt install gnat gprbuild');
+        await exec.exec(`git clone -b ${repo_branch} ${repo_url} ${alire_src}`);
+        process.chdir(alire_src);
+        await exec.exec(`git submodule update --init --recursive`);
+        await exec.exec(`gprbuild -j0 -p -XSELFBUILD=False -P alr_env.gpr`);
+
+        core.addPath(path.join(process.cwd(), 'bin'));
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
