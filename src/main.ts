5d3b383646b7ea628098638aee6eeb598d6b092a
Fabien Chouteau <chouteau@adacore.com>
2020-10-08 18:41:37 +0200

    New version to download stable build from GitHub

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,35 @@
 import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as tc from '@actions/tool-cache';
 const path = require("path");
 
-
-// This comes from https://github.com/actions/toolkit/blob/master/packages/tool-cache/src/tool-cache.ts
-// It would be nice to have an API to get the tempDirectory path.
-
-const IS_WINDOWS = process.platform === 'win32'
-const userAgent = 'actions/tool-cache'
-
-// On load grab temp directory and cache directory and remove them from env (currently don't want to expose this)
-let tempDirectory: string = process.env['RUNNER_TEMP'] || ''
-let cacheRoot: string = process.env['RUNNER_TOOL_CACHE'] || ''
-// If directories not found, place them in common temp locations
-if (!tempDirectory || !cacheRoot) {
-  let baseLocation: string
-  if (IS_WINDOWS) {
-    // On windows use the USERPROFILE env variable
-    baseLocation = process.env['USERPROFILE'] || 'C:\\'
-  } else {
-    if (process.platform === 'darwin') {
-      baseLocation = '/Users'
-    } else {
-      baseLocation = '/home'
-    }
-  }
-  if (!tempDirectory) {
-    tempDirectory = path.join(baseLocation, 'actions', 'temp')
-  }
-  if (!cacheRoot) {
-    cacheRoot = path.join(baseLocation, 'actions', 'cache')
-  }
-}
-
-const repo_url="https://github.com/alire-project/alire.git";
-const repo_branch="master";
-const alire_src=path.join(tempDirectory, "alire_src");
+const ver="0.7"
+const base_url="https://github.com/alire-project/alire/releases/download/";
+const linux_url = base_url + "v" + ver + "/alr-" + ver + "-bin-ubuntu-lts.zip";
+const darwin_url= base_url + "v" + ver + "/alr-" + ver + "-bin-macos.zip";
+const win32_url = base_url + "v" + ver + "/alr-" + ver + "-bin-windows.zip";
 
 async function run() {
     try {
-        if (process.platform != 'linux') {
-            core.setFailed(`Alire is not available on ${process.platform} yet`);
-            return;
+        var url;
+        switch (process.platform) {
+        case 'linux':
+            url = linux_url;
+            break;
+        case 'darwin':
+            url = darwin_url;
+            break;
+        case 'win32':
+            url = win32_url;
+            break;
+        default:
+            throw new Error('Unknown platform ' + process.platform);
         }
 
-        await exec.exec('sudo apt install gnat gprbuild');
-        await exec.exec(`git clone -b ${repo_branch} ${repo_url} ${alire_src}`);
-        process.chdir(alire_src);
-        await exec.exec(`git submodule update --init --recursive`);
-        await exec.exec(`gprbuild -j0 -p -XSELFBUILD=False -P alr_env.gpr -cargs -fPIC`);
+        const dlFile = await tc.downloadTool(url);
+        tc.extractZip(dlFile, "alire_install");
 
-        core.addPath(path.join(process.cwd(), 'bin'));
+        core.addPath(path.join(process.cwd(), "alire_install", 'bin'));
+
     } catch (error) {
         core.setFailed(error.message);
     }
