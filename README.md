fce07396edf9986cd79957122d2524c9437a99c4
Alejandro R. Mosteo <amosteo@unizar.es>
2020-12-18 18:29:27 +0100

    New latest-devel branch to use alr from master

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ GitHub action to setup Alire, the Ada/SPARK package manager.
 
 ## Usage
 
+Requires a valid GNAT toolchain already set up in the workflow (for the
+`latest-devel` branch only).
+
 Add this line to your workflow steps:
 ```
-    - uses: alire-project/setup-alire@dev
+    - uses: alire-project/setup-alire@latest-devel
 ```
 
 The command line tool `alr` will be available in `PATH`.
--- src/main.ts
+++ src/main.ts
@@ -2,49 +2,12 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 const path = require("path");
 
-
-// This comes from https://github.com/actions/toolkit/blob/master/packages/tool-cache/src/tool-cache.ts
-// It would be nice to have an API to get the tempDirectory path.
-
-const IS_WINDOWS = process.platform === 'win32'
-const userAgent = 'actions/tool-cache'
-
-// On load grab temp directory and cache directory and remove them from env (currently don't want to expose this)
-let tempDirectory: string = process.env['RUNNER_TEMP'] || ''
-let cacheRoot: string = process.env['RUNNER_TOOL_CACHE'] || ''
-// If directories not found, place them in common temp locations
-if (!tempDirectory || !cacheRoot) {
-  let baseLocation: string
-  if (IS_WINDOWS) {
-    // On windows use the USERPROFILE env variable
-    baseLocation = process.env['USERPROFILE'] || 'C:\\'
-  } else {
-    if (process.platform === 'darwin') {
-      baseLocation = '/Users'
-    } else {
-      baseLocation = '/home'
-    }
-  }
-  if (!tempDirectory) {
-    tempDirectory = path.join(baseLocation, 'actions', 'temp')
-  }
-  if (!cacheRoot) {
-    cacheRoot = path.join(baseLocation, 'actions', 'cache')
-  }
-}
-
 const repo_url="https://github.com/alire-project/alire.git";
 const repo_branch="master";
-const alire_src=path.join(tempDirectory, "alire_src");
+const alire_src="alire_install"; // To match the latest-stable install path
 
 async function run() {
     try {
-        if (process.platform != 'linux') {
-            core.setFailed(`Alire is not available on ${process.platform} yet`);
-            return;
-        }
-
-        await exec.exec('sudo apt install gnat gprbuild');
         await exec.exec(`git clone -b ${repo_branch} ${repo_url} ${alire_src}`);
         process.chdir(alire_src);
         await exec.exec(`git submodule update --init --recursive`);
