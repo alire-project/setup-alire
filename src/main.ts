021c21def963decc30eca254f402fec94f4a0c31
Alejandro R. Mosteo <amosteo@unizar.es>
2021-02-24 10:46:48 +0100

    Update alr stable version, dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as tc from '@actions/tool-cache';
 const path = require("path");
 
-const ver="0.7.0"
+const ver="1.0.0"
 const base_url="https://github.com/alire-project/alire/releases/download/";
 const linux_url = base_url + "v" + ver + "/alr-" + ver + "-bin-linux.zip";
 const darwin_url= base_url + "v" + ver + "/alr-" + ver + "-bin-macos.zip";
