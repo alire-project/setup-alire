238b889b5a4a7ecbc60534c38630532c92752451
Alejandro R. Mosteo <amosteo@unizar.es>
2021-09-01 11:32:39 +0200

    Use auxiliary branch to test

---
+++ scripts/matching-branches.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check that whichever commit we are on is the same as 'merging-devel'
+
+target=devel
+
+commit1=$(git log origin/merging-devel -1 --no-decorate --oneline | cut -f1 -d' ')
+commit2=$(git log -1 --no-decorate --oneline | cut -f1 -d' ')
+
+if [ "$commit1" == "$commit2" ]; then
+    echo Commits match
+    exit 0
+else
+    echo Commits do not match: ensure merging-devel points to your last commit
+    exit 1
+fi
