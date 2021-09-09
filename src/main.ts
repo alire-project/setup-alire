190327a55079cf930206fc29cca8fad3165b9469
Alejandro R. Mosteo <amosteo@unizar.es>
2021-09-09 09:30:05 +0200

    Fix for new typescript behavior post 4.0
    According to https://stackoverflow.com/a/69028217/761390

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,12 @@ async function run() {
         }
         console.log("SUCCESS");
     } catch (error) {
-        core.setFailed(error.message);
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+        else {
+            core.setFailed("Unknown error situation");
+        }
     }
 }
 
