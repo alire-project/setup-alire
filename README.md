7cd956702bff976662ca2d448887b5d093c44403
Alejandro R Mosteo <alejandro@mosteo.com>
2021-09-14 13:35:05 +0200

    CI improvements (ensure the actual changes are being tested) (#18)
    * Disable assistant post-installation
    
    * Attempt to fix the CI sanity check
    
    * Use auxiliary branch to test
    
    * Clarify the need for merging-devel
    
    * Fix PR commit
    
    * A bit of debug output
    
    * Use long commit
    
    * Show built version
    
    * Simplify testing by forcing the submission branch
    
    * Remove now unused script
    
    * Revert disabling of assistant on first run
    
    * Typo
    
    * Build the latest changes
    
    * Run `alr version` non-interactive
    
    * Fix for new typescript behavior post 4.0
    
    According to https://stackoverflow.com/a/69028217/761390
    
    * Parameters to control the preferred toolchain
    
    * Document new parameters
    
    * Fix bug in which disabling the assistant still used --select
    
    * Use --select or --install as appropriate

--- README.md
+++ README.md
@@ -12,4 +12,13 @@ Add this line to your workflow steps:
     - uses: alire-project/setup-alire@latest-devel
 ```
 
+Optional parameters are
+```
+    - with:
+      - toolchain: "gnat_native gprbuild<2000"
+      # Arguments to `alr toolchain`. Use `--disable-assistant` for pre-1.1 behavior
+      - toolchain_dir: ""
+      # Location where toolchains will be installed"
+```
+
 The command line tool `alr` will be available in `PATH`.
--- src/main.ts
+++ src/main.ts
@@ -11,25 +11,42 @@ async function run() {
         await exec.exec(`git clone -b ${repo_branch} ${repo_url} ${alire_src}`);
         process.chdir(alire_src);
         await exec.exec(`git submodule update --init --recursive`);
-        
+
         if (process.platform == "darwin") {
             process.env.OS = "macOS"
             console.log("NOTE: Configuring ENV for macOS")
         } else {
             console.log("NOTE: Configuring ENV for Linux/Windows")
         }
-        
+
         await exec.exec(`gprbuild -j0 -p -XSELFBUILD=False -P alr_env.gpr -cargs -fPIC`);
 
         core.addPath(path.join(process.cwd(), 'bin'));
 
-        // For some reason, this makes the action step to never finish on
-        // Windows
-        // console.log("Built version:");
-        // await exec.exec(`alr -n version`);
+        const tool_args : string = core.getInput('toolchain');
+        const tool_dir  : string = core.getInput('toolchain_dir');
+
+        if (tool_args.length > 0) {
+            if (tool_dir.length == 0) {
+                await exec.exec(`alr -n toolchain ${tool_args != "--disable-assistant" ? "--select " : ""} ${tool_args}`);
+            } else {
+                await exec.exec(`alr -n toolchain --install ${tool_args} --install-dir ${tool_dir}`);
+            }
+        }
+
+        // For some reason, this makes the action step to never finish on Windows
+        if (process.platform != "win32") {
+            console.log("Built version:");
+            await exec.exec(`alr -n version`);
+        }
         console.log("SUCCESS");
     } catch (error) {
-        core.setFailed(error.message);
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+        else {
+            core.setFailed("Unknown error situation");
+        }
     }
 }
 
